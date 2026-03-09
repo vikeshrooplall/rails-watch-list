9218fd462272e88ffd83b6542a9f032f59c94727
vikeshrooplall <rooplallv50@gmail.com>
2026-03-09 20:19:21 +0400

    coded frontend for lists#index, added destroy action for list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,6 +20,12 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, notice: "List was successfully deleted."
+  end
+
   private
   def list_params
     params.require(:list).permit(:name)
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "lists#index"
 
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
+  
+  delete 'remove_list/:id', to: 'lists#destroy', as: 'remove_list'
 end
