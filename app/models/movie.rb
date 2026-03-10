645b00c30927cb0432941becf4f5da9c67d743d4
vikeshrooplall <rooplallv50@gmail.com>
2026-03-10 12:12:13 +0400

    removed dependent destroy for bookmarks in movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks, dependent: :destroy
+  has_many :bookmarks
   has_many :lists, through: :bookmarks
 
   validates :title, presence: true, uniqueness: true
